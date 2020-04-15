f604705cce0189111b2617d67f7cdab97a062b63
Linus <wendefelix1998@gmail.com>
2020-04-15 17:11:05 +0200

    Use GUI by default

--- Emulator/src/me/emu6502/emulator/Main.kt
+++ Emulator/src/me/emu6502/emulator/Main.kt
@@ -5,24 +5,25 @@ import tornadofx.launch
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    if(args.contains("--gui") || args.contains("-g")) {
-        // Launch GUI
-        Console.writeLine("Launching GUI...")
-        launch<EmulatorApp>(args)
-        exitProcess(0)
+    if(args.contains("--cli") || args.contains("-i")) {
+        // Launch in console
+        val emulator = Emulator(
+                clear = { Console.clear() },
+                defineCommand = { name, displayName, desc -> Console.completer.addCommand(name, displayName, desc) },
+                reportError = { Console.writeLine(it); Console.readKey() },
+                updateScreen = { it.screenshot() },
+                write = { Console.write(it) },
+                writeLine = { Console.writeLine(it) }
+        )
+
+        emulator.printStatus()
+        while(true)
+            emulator.executeDebuggerCommand(Console.readLine("> "))
     }
 
-    // Launch in console
-    val emulator = Emulator(
-            clear = { Console.clear() },
-            defineCommand = { name, displayName, desc -> Console.completer.addCommand(name, displayName, desc) },
-            reportError = { Console.writeLine(it); Console.readKey() },
-            updateScreen = { it.screenshot() },
-            write = { Console.write(it) },
-            writeLine = { Console.writeLine(it) }
-    )
-
-    emulator.printStatus()
-    while(true)
-        emulator.executeDebuggerCommand(Console.readLine("> "))
+    // Launch GUI
+    Console.writeLine("Launching GUI...")
+    Console.writeLine("To use the console mode, pass the --cli flag.")
+    launch<EmulatorApp>(args)
+    exitProcess(0)
 }
