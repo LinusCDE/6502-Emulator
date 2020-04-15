6e765a84865cf153c40b4147437b52bb114ae631
Linus <wendefelix1998@gmail.com>
2020-04-15 17:32:05 +0200

    Allow out of bound memory writes

--- Emulator/src/me/emu6502/emulator/DirectBitmap.kt
+++ Emulator/src/me/emu6502/emulator/DirectBitmap.kt
@@ -11,7 +11,11 @@ class DirectBitmap(val width: Int, val height: Int) {
     val image: BufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
     operator fun get(x: Int, y: Int) = Color(image.getRGB(x, y)) // Should also do alpha
-    operator fun set(x: Int, y: Int, color: Color) = image.setRGB(x, y, color.rgb)
+    operator fun set(x: Int, y: Int, color: Color) {
+        if(x < 0 || y < 0 || x >= width || y >= height)
+            return // Ignore setting out of bound coordinates
+        image.setRGB(x, y, color.rgb)
+    }
 
     @Throws(IOException::class)
     fun save(fileName: String) = ImageIO.write(image, "bmp", File(fileName))
--- Lib6502/src/me/emu6502/lib6502/ROM.kt
+++ Lib6502/src/me/emu6502/lib6502/ROM.kt
@@ -16,7 +16,6 @@ class ROM(size: UShort, start: UShort): Device(start, (start + size minusSigned 
 
     override fun setData(data: UByte, address: UShort) {
         System.err.println("WARN: Attempt to write to ROM at $address!!! Write ignored!")
-        // TODO: Add exception (or does the hardware forgive this?)
     }
 
     override fun getData(address: UShort): UByte = if(request(address)) memory[(address - start).int] else 0.ubyte
