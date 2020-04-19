fb0224416bb3c327432e2bf2bf342cfa74402a06
Linus <wendefelix1998@gmail.com>
2020-04-19 02:40:31 +0200

    Fix wrong VT100 codes

--- KotlinUtils/src/me/emu6502/kotlinutils/vt100/VT100Display.kt
+++ KotlinUtils/src/me/emu6502/kotlinutils/vt100/VT100Display.kt
@@ -1,15 +1,12 @@
 package me.emu6502.kotlinutils.vt100
 
-enum class VT100Display: VT100Attribute {
+enum class VT100Display(override val attribute: Int): VT100Attribute {
 
-    RESET_ALL_ATTRIBUTES,
-    BRIGHT,
-    DIM,
-    UNDERSCORE,
-    BLINK,
-    REVERSE,
-    HIDDEN;
-
-    override val attribute: Int = ordinal // 0 ...
-
+    RESET_ALL_ATTRIBUTES(0),
+    BRIGHT(1),
+    DIM(2),
+    UNDERSCORE(4),
+    BLINK(5),
+    REVERSE(7),
+    HIDDEN(8);
 }
