b9fde725020b17c47e72e7839bb2b2c73fb09fb6
Linus <wendefelix1998@gmail.com>
2020-05-11 22:39:30 +0200

    Add title to external AssemblerView

--- Emulator/src/me/emu6502/emulator/ui/view/AssemblerWindow.kt
+++ Emulator/src/me/emu6502/emulator/ui/view/AssemblerWindow.kt
@@ -1,19 +1,18 @@
 package me.emu6502.emulator.ui.view
 
 import javafx.stage.Modality
+import me.emu6502.emulator.ui.controller.AssemblerController
 import me.emu6502.emulator.ui.controller.MainController
 import tornadofx.*
 
 class AssemblerWindow: Fragment() {
 
     val mainController: MainController by inject()
+    val asmController: AssemblerController by inject()
 
     override fun onDock() {
         root.center = find<AssemblerView>().root
-        openWindow(modality = Modality.NONE)!!.apply {
-            width = 400.0
-            height = 600.0
-        }
+        openWindow(modality = Modality.NONE)
     }
 
     override fun onUndock() {
@@ -22,6 +21,16 @@ class AssemblerWindow: Fragment() {
         }
     }
 
-    override val root = borderpane { }
+    override val root = borderpane {
+        titleProperty.bind(asmController.openedAssemblyFileProperty.stringBinding { file ->
+            val programName = find<AssemblerView>().title.split(" - ")[0] // From title of main window
+            if(file == null)
+                "$programName - Assembler - Neue Datei"
+            else
+                "$programName - Assembler - ${file.name}"
+        })
+        prefWidth = 400.0
+        prefHeight = 600.0
+    }
 
 }
